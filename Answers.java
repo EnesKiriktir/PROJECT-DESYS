f641132fe60a9f730373ff294e9a13cea5b092ee
EnesKiriktir <38852040+EnesKiriktir@users.noreply.github.com>
2018-04-30 09:58:17 +0200

    Update Answers.java

--- Answers.java
+++ Answers.java
@@ -1,7 +1,7 @@
 
 public class Answers {
 	String a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20;
-
+	// KASJDFGDASJGFDADKJGDFGHKGFH
 	public Answers(String a1, String a2, String a3, String a4, String a5, String a6, String a7, String a8, String a9, String a10, String a11, String a12,
 			String a13, String a14, String a15, String a16, String a17, String a18, String a19, String a20) {
 		this.a1 = a1;
